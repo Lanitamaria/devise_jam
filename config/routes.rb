edb7b412ffc40f7deb756d16b7e06dad31bfc790
Anita Espinoza <anita.espinoza@equifax.com>
2020-08-14 22:53:34 -0400

    add devise to user and stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
+  devise_for :users
   resources :stories
   root 'stories#index'
+
+  get  'my_stories', to: 'stories#my_stories'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
